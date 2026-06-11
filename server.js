fbb8e6d5592172d773999ec0db988ed709fc7bee
Software Oficial <software-oficial@github.com>
2026-06-11 04:56:36 -0300

    fix(server): update catch-all route to regex for Express 5 compatibility

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Cualquier otra ruta devuelve el index.html (Single Page Application)
-app.get('*', (req, res) => {
+app.get(/.*$/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
